f847a8fa57e41a57fd9d1a86c6264c3823eb836f
augusto barbosa <augustobarbosa1988@gmail.com>
2023-02-04 08:53:14 -0300

    feat: função exist_word, que busca por uma palavra específica dentro da fila de arquivos processados

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,38 @@
+# from ting_file_management.queue import Queue
+# from ting_file_management.file_process import process
+
+
+def list_contains(list, word):
+    list_content = []
+    for index in range(len(list)):
+        if word.lower() in list[index].lower():
+            list_content.append(
+                {"linha": index + 1}
+            )
+
+    return list_content
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrence_list = []
+    for index in range(len(instance)):
+        item_in_queue = instance.search(index)
+        list_content = list_contains(item_in_queue['linhas_do_arquivo'], word)
+        if len(list_content) > 0:
+            occurrence_list.append({
+                "palavra": word,
+                "arquivo": item_in_queue['nome_do_arquivo'],
+                "ocorrencias": list_content
+            })
+
+    return occurrence_list
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+# if __name__ == "__main__":
+#     queue1 = Queue()
+#     process("statics/nome_pedro.txt", queue1)
+#     print('Resposta abaixo:')
+#     print(exists_word("pedro", queue1))
